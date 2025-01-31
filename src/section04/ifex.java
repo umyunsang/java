1947993caa252c071d1a3e09a6cf2d433661afb8
umyunsang <dbstkd5865@gmail.com>
2025-02-01 08:13:08 +0900

    notes

---
+++ src/section04/ifex.java
@@ -0,0 +1,12 @@
+package section04;
+
+public class ifex {
+    public static void main(String[] args) {
+        int result = 0;
+        if (true){
+            result = 3;
+        }
+        
+        System.out.println(result);
+    }
+}
