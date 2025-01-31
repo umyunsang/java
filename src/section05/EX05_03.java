4e4ad29799174601d886e0a5ed7077f80ec888e7
umyunsang <dbstkd5865@gmail.com>
2025-02-01 08:39:49 +0900

    구구단 출력

--- src/section05/EX05_03.java
+++ src/section05/EX05_03.java
@@ -2,8 +2,14 @@ package section05;
 
 public class EX05_03 {
     public static void main(String[] args) {
-        for(int i=2; i<=9; i++){
-            for(int j=1; j<=9; j++){
+        // for(int i=2; i<=9; i++){
+        //     for(int j=1; j<=9; j++){
+        //         System.out.print(i+"x"+j+"="+(i*j)+"\t");
+        //     }
+        //     System.out.println();
+        // }
+        for(int j=1; j<=9; j++){
+            for(int i=2; i<=9; i++){
                 System.out.print(i+"x"+j+"="+(i*j)+"\t");
             }
             System.out.println();
