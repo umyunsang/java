e521cc965928e169ddfb4325bb6c2cfbfa4e18ff
umyunsang <dbstkd5865@gmail.com>
2025-02-01 12:02:48 +0900

    정적 멤버

---
+++ src/section07/Student.java
@@ -0,0 +1,9 @@
+package section07;
+
+public class Student {
+    static String schoolName = "코리아 고등학교";   // 정적 멤버 선언
+
+    static void goToSchool(){   // 정적 메서드 선언언
+        System.out.println("학교에 갑니다.");
+    }
+}
