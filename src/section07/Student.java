f7150a065b62c5e3c288a56f6714547d33a43d02
umyunsang <dbstkd5865@gmail.com>
2025-02-01 12:04:38 +0900

    Student 객체체

--- src/section07/Student.java
+++ src/section07/Student.java
@@ -2,8 +2,13 @@ package section07;
 
 public class Student {
     static String schoolName = "코리아 고등학교";   // 정적 멤버 선언
+    String studentName; // 인스턴스 필드 선언
 
-    static void goToSchool(){   // 정적 메서드 선언언
+    static void goToSchool(){   // 정적 메서드 선언
         System.out.println("학교에 갑니다.");
+    }
+
+    void hello() {
+        System.out.println("안녕하세요, 제 이름은 "+ studentName + "입니다.");
     }
 }
