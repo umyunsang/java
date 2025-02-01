e193e2ca9796ba1462152dd5d7c7df19bc183c83
umyunsang <dbstkd5865@gmail.com>
2025-02-01 12:05:45 +0900

    정적 멤버

---
+++ src/section07/EX07_13.java
@@ -0,0 +1,9 @@
+package section07;
+
+public class EX07_13 {
+    public static void main(String[] args) {
+        System.out.println(Student.schoolName);     // 정적 필드 호출
+        Student.goToSchool();      // 정적 메소드 호출
+    
+    }
+}
