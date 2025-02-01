690fe0221e1e28a9f853366a2796cb5cdbe13990
umyunsang <dbstkd5865@gmail.com>
2025-02-01 12:19:30 +0900

    메서드 선언 및 호출

---
+++ src/section08/EX08_01.java
@@ -0,0 +1,12 @@
+package section08;
+
+public class EX08_01 {
+    public static void main(String[] args) {
+        printHello();
+    }
+
+    static void printHello(){
+        System.out.println("안녕하세요");
+        System.out.println("만나서 반갑습니다.");
+    }
+}
