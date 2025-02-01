3b3274e1ef8a04a25f4391017b8b92c8419c0287
umyunsang <dbstkd5865@gmail.com>
2025-02-01 11:55:00 +0900

    Car媛앹껜

--- src/section07/Car.java
+++ src/section07/Car.java
@@ -1,5 +1,10 @@
 package section07;
 
 public class Car {
-    int wheel = 4;  // 클래스 필드 선언 및 초기화화
+    int wheel = 4;  // 클래스 필드 선언 및 초기화
+
+    void ride(){    // 메서드 선언
+        System.out.println("달립니다.");
+        System.out.println("씽씽씽씽");
+    }
 }
